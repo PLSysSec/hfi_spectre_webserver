032c3ef080f42d616b4329e7616f083f5069ad2f
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2020-06-17 13:47:56 -0700

    some more helpful error messages in a few cases

--- src/wasm_module.rs
+++ src/wasm_module.rs
@@ -18,9 +18,9 @@ pub fn create_wasm_instance<P: AsRef<Path>>(
     let module = if aslr {
         let filename = module_name + ".so";
         let path = dir.as_ref().join(filename);
-        DlModule::load_aslr(&path, true).unwrap()
+        DlModule::load_aslr(&path, true).unwrap_or_else(|e| panic!("Failed to load module from path {:?} with aslr: {}", &path, e))
     } else {
-        service_directory::get_module(&module_name).unwrap()
+        service_directory::get_module(&module_name).unwrap_or_else(|| panic!("Failed to get module {:?}", &module_name))
     };
     let wasi_ctx = {
         let mut builder = lucet_wasi::WasiCtxBuilder::new();
